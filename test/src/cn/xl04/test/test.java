4187cb05accc4aced746ab8fcfc639a60d6f1fbe
yangfxl <3262115653@qq.com>
2023-07-26 11:27:12 +0800

    update

--- test/src/cn/xl04/test/test.java
+++
@@ -1,7 +0,0 @@
-package cn.xl04.test;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello,world");
-    }
-}
--- test/src/cn/xl04/test/test2.java
+++
@@ -1,7 +0,0 @@
-package cn.xl04.test;
-
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println("nb");
-    }
-}
